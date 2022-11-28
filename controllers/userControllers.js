6340d1b47d087bc0403ef87837f626c302aaf69a
quoctung <tungnq2606@gmail.com>
2022-11-28 16:13:08 +0700

    Validate old password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,69 +1,73 @@
 const User = require("../models/User");
-<<<<<<< HEAD
 const bcrypt = require("bcryptjs");
-=======
->>>>>>> 7ef4a5a54f3bf99b4793382b54883414515bbc87
 
 const userController = {
-    //Get all users
-    getAllUsers: async (req, res) => {
-        try {
-            const user = await User.find().populate("devices");
-            res.status(200).json(user);
+  //Get all users
+  getAllUsers: async (req, res) => {
+    try {
+      const user = await User.find().populate("devices");
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
+  //Get user by id
+  getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id).populate("devices");
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    //Get user by id
-    getUserById: async (req, res) => {
-        try {
-            const user = await User.findById(req.params.id).populate("devices");
-            res.status(200).json(user);
+  //Delete user
+  deleteUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Xoá tài khoản thành công");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
+  //Update user
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      await user.updateOne({ $set: req.body });
+      res.status(200).json("Cập nhật thành công");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    //Delete user
-    deleteUser: async (req, res) => {
-        try {
-            const user = await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("Xoá tài khoản thành công");
-
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
-
-    //Update user
-    updateUser: async (req, res) => {
-        try {
-            const user = await User.findById(req.params.id);
-            await user.updateOne({ $set: req.body });
-            res.status(200).json("Cập nhật thành công");
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
-
-<<<<<<< HEAD
-    //Update pass
-    updatePassUser: async (req, res) => {
-        try {
-            const salt = await bcrypt.genSalt(10);
-            const hashed = await bcrypt.hash(req.body.password, salt);
-            const user = await User.findByIdAndUpdate( {_id: req.params.id}, {$set:{password: hashed }});
-            res.status(200).json("Đổi mật khẩu thành công");
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
-
-=======
->>>>>>> 7ef4a5a54f3bf99b4793382b54883414515bbc87
-}
+  //Update pass
+  updatePassUser: async (req, res) => {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashed = await bcrypt.hash(req.body.password, salt);
+      const user = await User.findOne({
+        _id: req.params.id,
+      });
+      const comparePwd = await bcrypt.compare(
+        req.body.oldPassword,
+        user.password
+      );
+      if (comparePwd) {
+        await User.findByIdAndUpdate(
+          { _id: req.params.id },
+          { $set: { password: hashed } }
+        );
+        res.status(200).json("Đổi mật khẩu thành công");
+      } else {
+        res.status(400).json("Mật khẩu cũ không đúng");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
 
 module.exports = userController;
